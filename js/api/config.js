75f782d6a2c1c8bafabb1e45e0430deb177a91a2
Your Name <you@example.com>
2024-03-10 19:12:28 +0100

    Aggiunto trendigData

--- js/api/config.js
+++ js/api/config.js
@@ -1,10 +1,15 @@
+// import del package che mi rende disponibili le variabili d'ambiente
+import dotenv from "dotenv";
+//carica le variabili d'ambiente del file .env
+dotenv.config();
+
 export const BASE_URL = "https://api.themoviedb.org/3/";
 
 export const OPTS = {
     method: "GET",
     headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDMzMjM5MTFlNDNmMTZhODBlOGY4ZjJlOGUwMTY1ZiIsInN1YiI6IjY1ZTg0YzI2M2ZlMTYwMDE3YjVkN2Y5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s-8ck3PUXG5BIVd_ZwlC0zH72wP6LK65MX1apv3OjJA",
+        Authorization: `Bearer ${process.env.API_KEY}`,
 
     },
 };
--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 import { getTrending, getTrendingMovies, getSerchPerson } from "./api/trendingGetter.js";
+ 
+const trendingData = getTrending().then(data => {
+    data.results.forEach(movie => {
+        console.log("ID: " + movie.id);
+        console.log("Poster_path: " + movie.poster_path);
+        console.log("Title: " + movie.title);
+        console.log("Vote_average: " + movie.vote_average);
+        console.log("Media_type: " + movie.media_type );
+        console.log("Release_date: " + movie.release_date + "\n");
+    });
+}); 
 
 getTrending();
 getTrendingMovies();
 getSerchPerson();
+
